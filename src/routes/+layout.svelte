6de327cbcec58166b95e84faa69782f4affa4692
zy <zhuyongdev@163.com>
2025-06-16 22:14:08 +0800

    统计代码

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,26 @@
   import Navbar from "$lib/components/Siderbar.svelte";
   import Sidebar from "$lib/components/Navbar.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import { PUBLIC_CLARITY_ID } from "$env/static/public";
+  import { onMount } from "svelte";
 
   let { children } = $props();
 
   import { ModeWatcher } from "mode-watcher";
   import { open } from "$lib/store/sidebar.js";
-  import { cn } from "$lib/utils";
+
+  onMount(() => {
+    const script = document.createElement("script");
+    script.type = "text/javascript";
+    script.innerHTML = `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", ${PUBLIC_CLARITY_ID});
+  `;
+    document.head.appendChild(script);
+  });
 
   let status = $state();
 
