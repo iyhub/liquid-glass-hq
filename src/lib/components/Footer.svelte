3755ee58061b0c48aa0bdc1ef6c536dac52f687b
iyhub <zhuyongdev@163.com>
2025-10-13 10:37:11 +0800

    fix ui & add links

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -2,17 +2,24 @@
   const year = $state(new Date().getFullYear());
 </script>
 
-<footer class="bg-secondary text-center py-4 flex flex-col md:flex-row items-center gap-2">
-  <ul>
+<footer class="bg-secondary text-center py-4 flex flex-col items-center gap-2">
+  <ul
+    class="flex flex-col md:flex-row items-center gap-4 underline underline-offset-4 text-gray-600 text-xs"
+  >
     <li>
       <a href="https://imagetoimage.app/" rel="dofollow"> Image to Image AI </a>
     </li>
     <li>
       <a href="https://tossfly.com/" rel="dofollow">Tossfly</a>
     </li>
+    <li>
+      <a href="https://imagedescriber.cc/" rel="dofollow"
+        >Image Describer AI
+      </a>
+    </li>
   </ul>
 
   <p class="text-sm text-muted-foreground">
-    &copy; {year} Meet Liquid. All rights reserved.
+    &copy; {year} LiquidGlassHQ. All rights reserved.
   </p>
 </footer>
